1305fbfc1958238fc20b1e92152858296f31009b
danlauby <dmlauby@gmail.com>
2017-01-11 13:25:31 -0800

    add form functionality with jQuery by appending inputs individually

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(function() {
+  $('#blanks form').submit(function(e) {
+    var person1Input = $('input#person1').val();
+    var person2Input = $('input#person2').val();
+    var animalInput = $('input#animal').val();
+    var exclamationInput = $('input#exclamation').val();
+    var verbInput = $('input#verb').val();
+    var nounInput = $('input#noun').val();
+
+    $(".person1").append(person1Input);
+    $(".person2").append(person2Input);
+    $(".animal").append(animalInput);
+    $(".exclamation").append(exclamationInput);
+    $(".verb").append(verbInput);
+    $(".noun").append(nounInput);
+
+    $('#story').show();
+
+    e.preventDefault();
+  });
+});
